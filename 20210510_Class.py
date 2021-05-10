9b00bcf91ce3e2d92a6649d86967d5a35e15bf3b
Prime0220 <zmwhwa@naver.com>
2021-05-10 15:51:07 +0900

    dayTwelve

---
+++ 20210510_Class.py
@@ -0,0 +1,30 @@
+import math 
+
+print(math.pi)
+print(math.sqrt(4.0))
+
+import math as m
+
+print(m.sqrt(4.0))
+
+from math import pi
+print(pi)
+
+from math import *
+print(pi)
+print(sqrt(4.0))
+
+from math import sqrt as s
+print(s(4.0))
+
+from math import pi as p, sqrt as s 
+print(p)
+print(s(2.0))
+
+import sys
+
+print(sys.builtin_module_names)
+
+import masth 
+
+print(dir(math))
